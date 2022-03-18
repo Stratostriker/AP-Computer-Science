3fa3a9586e350429ac38ec5723531454831de2df
Alan Gu <57688846+Stratostriker@users.noreply.github.com>
2022-03-18 20:39:29 +0000

    BinarySearch

--- BinarySearch.java
+++ BinarySearch.java
@@ -17,21 +17,10 @@ public class BinarySearch {
     for (int i = 1; i < length; i++) {
       result[i] = result[i-1] + randGen.nextInt(3);
     }
+    return result;
   }
   
   public static int binarySearch(int[] array, int key) {
     return 6;
-  }
-  
-  public static void main(String[] args) {
-    int[] test1 = generate(10, 0 , 10);
-    int[] test2 = generateSorted(10);
-    for(int i = 0; i < 10; i++) {
-      System.out.print(test1[i] + " ");
-    }
-    System.out.println("");
-    for(int i = 0; i < 10; i++) {
-      System.out.print(test2[i] + " ");
-    }
   }
 }
