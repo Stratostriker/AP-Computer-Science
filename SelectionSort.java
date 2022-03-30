ce63de278e0a64572ffb2b9947b4a5793e3cde16
Alan Gu <57688846+Stratostriker@users.noreply.github.com>
2022-03-30 14:21:31 +0000

    SelectionSort

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,6 +1,21 @@
 public class SelectionSort {
   public static void main(String[] args {
-    t
+    for (int i = 0; i < array.length; i++) {
+            int min = Integer.MAX_VALUE;
+            int minIndex = 0;
+            int j = i;
+            // find smallest value of array and its index
+            for (; j < array.length; j++) {
+                if (array[j] < min) {
+                    min = array[j];
+                    minIndex = j;
+                }
+            }
+            // swap start of subarray and smallest number of subarray
+            array[minIndex] = array[i];
+            array[i] = min;
+        }
+        System.out.println(Arrays.toString(array));
   }
   public static void selectionSort(int[] array) {
     
